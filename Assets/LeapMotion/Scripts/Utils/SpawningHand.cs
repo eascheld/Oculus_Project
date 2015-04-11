fffba500cc31f8f3d92f1dd94b724f5b0a266468
Ethan Scheldorf <eascheld@ius.edu>
2015-04-10 20:12:31 -0400

    SpawningHands have become self-aware!!!
    So, my attempts to limit SpawningHand's seemingly infinite cubes have
    been foiled.  Despite an if-statement, the code continues to execute the
    spawn however many times it wants to (in this case, twice).  Beware the
    circle gesture!

--- Assets/LeapMotion/Scripts/Utils/SpawningHand.cs
+++ Assets/LeapMotion/Scripts/Utils/SpawningHand.cs
@@ -3,46 +3,71 @@ using System.Collections;
 using Leap;
 
 // Leap Motion hand script that detects shape gestures and spawns the appropriate GameObject
-class SpawningHand : MonoBehaviour
+class SpawningHand : HandController
 {
-    public GameObject spawnCube;
-    private Controller controller;
+    //public GameObject spawnCube;
     private Frame frame;
+    private bool spawned;
+    private int numSpawned = 0;
 
     void Start()
     {
-        controller = new Controller();
-        if (controller == null)
+        if (leap_controller_ == null)
         {
             Debug.LogWarning("Cannot connect to controller. Make sure you have Leap Motion v2.0+ installed.");
         }
         else
         {
-            controller.EnableGesture(Gesture.GestureType.TYPE_CIRCLE);
+            leap_controller_.EnableGesture(Gesture.GestureType.TYPE_CIRCLE);
+            leap_controller_.Config.SetFloat("Gesture.Circle.MinRadius", 10.0f);
+            //leap_controller_.Config.SetFloat("Gesture.Circle.MinArc", 2.0f*Mathf.PI);
+            leap_controller_.Config.Save();
         }
         
     }
 
     void Update()
     {
-        frame = controller.Frame();
-        if (frame.Gestures().Count > 0) {
-            foreach (Gesture gesture in frame.Gestures())
+        if (leap_controller_ == null)
+        {
+            return;
+        }
+
+        frame = GetFrame();
+        foreach (Gesture gesture in frame.Gestures())
+        {
+            switch (gesture.Type)
             {
-                switch (gesture.Type)
+                case(Gesture.GestureType.TYPE_CIRCLE):
                 {
-                    case(Gesture.GestureType.TYPE_CIRCLE):
+                    if (!spawned && numSpawned == 0)
                     {
                         Debug.Log("Got a circle gesture!");
-                        Instantiate(spawnCube);
-                        break;
+                        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                        numSpawned++;
+                        //CircleGesture theCircle = new CircleGesture(frame.Gestures()[0]); // new CircleGesture();
+                        //Leap.Vector v = theCircle.Center;
+                        cube.transform.localScale = new Vector3(1, 1, 1);
+                        cube.transform.position = new Vector3(0, 0.5f, 0);
+                        cube.AddComponent<GrabbableObject>();
+                        cube.AddComponent<Rigidbody>();
+                        cube.rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+                        cube.transform.renderer.materials[0].color = Color.blue;
+                        spawned = true;
+                        Debug.Log("Number Spawned: " + numSpawned);
                     }
-                    default:
-                    {
-                        break;
-                    }
+                    break;
+                }
+                default:
+                {
+                    break;
                 }
             }
         }
+    }
+
+    void FixedUpdate()
+    {
+        //spawned = false;
     }
 }
